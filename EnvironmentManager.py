8e107cb2311eb54b5601da1b6a75ceec8567fda3
Konstantin Kobs <mail@konstantinkobs.de>
2017-08-23 17:34:32 +0200

    added config and some code...

--- EnvironmentManager.py
+++ EnvironmentManager.py
@@ -1,11 +1,15 @@
-class EnvironmentManager:
+from config import config
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(config["logging_level"])
 
-    agents = []
+class EnvironmentManager:
 
     def __init__(self):
         return
 
-    def split_actions(self):
+    def split_actions(self, actions):
+
         return
 
     def merge_states(self):
