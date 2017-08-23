e222955b78dcd97e979220ba4aff9404ed387537
Konstantin Kobs <mail@konstantinkobs.de>
2017-08-23 15:53:35 +0200

    added AgentManager and EnvironmentManager

---
+++ AgentManager.py
@@ -0,0 +1,13 @@
+class AgentManager:
+
+    agents = []
+
+    def __init__(self):
+        # TODO: create agents
+        return
+
+    def merge_actions(self):
+        return
+
+    def split_states(self):
+        return
---
+++ EnvironmentManager.py
@@ -0,0 +1,12 @@
+class EnvironmentManager:
+
+    agents = []
+
+    def __init__(self):
+        return
+
+    def split_actions(self):
+        return
+
+    def merge_states(self):
+        return
